644fb6b581c5035e301803dfaf75b7f8e81e422d
Rohit Singh <rohitsinghpb4@gmail.com>
2026-02-19 13:31:33 +0530

    title added on header of moeals overview screen

--- app/screens/MealsOverviewScreen.tsx
+++ app/screens/MealsOverviewScreen.tsx
@@ -1,17 +1,28 @@
 import MealItems from "@/components/MealItems";
-import { MEALS } from "@/data/dummy-data";
+import { CATEGORIES, MEALS } from "@/data/dummy-data";
+import { useLayoutEffect } from "react";
 import { FlatList, StyleSheet, View } from "react-native";
 
-export default function MealsOverviewScreen({ route }: any) {
+export default function MealsOverviewScreen({ route, navigation }: any) {
   const catId = route.params.categoryId;
 
   const displayedMeals = MEALS.filter((mealItem) => {
     return mealItem.categoryIds.indexOf(catId) >= 0;
   });
 
+  useLayoutEffect(() => {
+    const categoryTitle = CATEGORIES.find(
+      (category) => category.id === catId,
+    )?.title;
+    navigation.setOptions({
+      title: categoryTitle,
+    });
+  }, [catId, navigation]);
+
   function renderMealItem(itemData: any) {
     return (
       <MealItems
+        id={itemData.item.id}
         title={itemData.item.title}
         imageUrl={itemData.item.imageUrl}
         duration={itemData.item.duration}
